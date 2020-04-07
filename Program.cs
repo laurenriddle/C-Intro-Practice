8ec930a4d89f27ebad48ceef381b60356d960baf
Lauren Riddle <lari7132@gmail.com>
2020-04-07 14:38:35 -0500

    finished dict exercise

--- Program.cs
+++ Program.cs
@@ -183,6 +183,63 @@ namespace bangazon
             }
 
 
+
+            // LIST OF DICTIONARIES ABOUT WORDS PRACTICE (EXTENTION OF THE LAST EXERCISE)
+
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+            // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> happyWord = new Dictionary<string, string>();
+            happyWord.Add("word", "happy");
+            happyWord.Add("definition", "feeling or showing pleasure or contentment");
+            happyWord.Add("part of speech", "adjective");
+            happyWord.Add("example sentence", "I am happy that we are having soup for dinner!");
+
+            dictionaryOfWords.Add(happyWord);
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach (Dictionary<string, string> dict in dictionaryOfWords)
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> wordData in dict)
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+            }
         }
     }
 }
